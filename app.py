9e83b5cb82ab23dfb21cee182cd50563e28a6430
Sifan Momin <114792773+sifanmomin@users.noreply.github.com>
2025-01-12 14:15:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,64 @@
-import pickle
 import streamlit as st
-import requests
 
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+import pickle
+import string
 
-def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
-    recommended_movie_posters = []
-    for i in distances[1:6]:
-        # fetch the movie poster
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
+import nltk
+import string # Import the string module
 
-    return recommended_movie_names,recommended_movie_posters
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+# Download necessary NLTK resources
+#nltk.download('punkt')
+#nltk.download('stopwords')
+#nltk.download('punkt_tab')
+
+ps = PorterStemmer()
 
 
-st.header('Movie Recommender System')
-movies = pickle.load(open('model/movie_list.pkl','rb'))
-similarity = pickle.load(open('model/similarity.pkl','rb'))
+def transfrom_text(text):
+  text = text.lower()
+  text = nltk.word_tokenize(text)
 
-movie_list = movies['title'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
+  y = []
+  for i in text:
+    if i.isalnum():
+      y.append(i)
 
-if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.beta_columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
+  text = y[:]
+  y.clear()
+  for i in text:
+    if i not in stopwords.words('english') and i not in string.punctuation:  # Now string.punctuation is accessible
+      y.append(i)
 
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+  text = y[:]
+  y.clear()
+  for i in text:
+    y.append(ps.stem(i))
+
+  return " ".join(y)
 
 
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title("Email/SMS Spam Classifier")
+input_sms = st.text_area("Enter the message")
+
+if st.button("Predict"):
 
 
+  # 1.preprocess
+  transformed_sms =transfrom_text(input_sms)
 
+  # 2 vectorize
+  vector_input = tfidf.transform([transformed_sms])
+  # 3 predict
+
+  result = model.predict(vector_input)[0]
+  # 4 display
+  if result == 1:
+      st.header("Spam")
+  else:
+      st.header("Not Spam")
